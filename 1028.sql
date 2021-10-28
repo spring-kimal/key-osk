5e0b5ca4693cb619cd4660360df38f3757063092
ass3027 <84160823+ass3027@users.noreply.github.com>
2021-10-28 19:24:20 +0900

    Update 1028.sql

--- 1028.sql
+++ 1028.sql
@@ -12,7 +12,36 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
+-- kimal 데이터베이스 구조 내보내기
+CREATE DATABASE IF NOT EXISTS `kimal` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `kimal`;
+
+-- 테이블 kimal.item 구조 내보내기
+CREATE TABLE IF NOT EXISTS `item` (
+  `itemId` int(10) NOT NULL,
+  `itemName` varchar(30) DEFAULT NULL,
+  `itemQuantity` int(10) DEFAULT NULL,
+  `itemPrice` int(10) DEFAULT NULL,
+  `itemindex` int(5) NOT NULL,
+  `category` varchar(10) NOT NULL,
+  `img` mediumblob NOT NULL,
+  PRIMARY KEY (`itemId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 -- 내보낼 데이터가 선택되어 있지 않습니다.
+
+-- 테이블 kimal.payment 구조 내보내기
+CREATE TABLE IF NOT EXISTS `payment` (
+  `payId` int(10) NOT NULL,
+  `itemId` int(10) DEFAULT NULL,
+  `Quantity` int(10) DEFAULT NULL,
+  `payPrice` int(10) DEFAULT NULL,
+  `payTime` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`payId`),
+  KEY `FK_itemid` (`itemId`),
+  CONSTRAINT `FK_itemid` FOREIGN KEY (`itemId`) REFERENCES `item` (`itemId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- 내보낼 데이터가 선택되어 있지 않습니다.
 
